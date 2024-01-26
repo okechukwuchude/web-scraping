f7db7be6df8658c1d39136a89cf526475fa93236
Okechukwu Chude <52760448+okechukwuchude@users.noreply.github.com>
2024-01-26 20:59:44 +0000

    update

---
+++ bookscraper/items.py
@@ -0,0 +1,15 @@
+# Define here the models for your scraped items
+#
+# See documentation in:
+# https://docs.scrapy.org/en/latest/topics/items.html
+
+import scrapy
+from scrapy import Item,Field
+
+
+class BookscraperItem(scrapy.Item):
+    title = Field()
+    price = Field()
+    upc = Field()
+    image_url = Field()
+    url = Field()
